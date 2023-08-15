79ad87754a5026bd0b7af8996dee461af0a27dc9
AbdullaAlMamun121 <hmdabdulla121@gmail.com>
2023-08-15 07:27:08 +0600

    initial setup

---
+++ index.js
@@ -0,0 +1,73 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+// middleware
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+// Mongodb template
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.uetnypa.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  maxPoolSize: 10,
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect((err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+    });
+
+    
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Job Hunter is running!");
+});
+
+app.listen(port, () => {
+  console.log("Listening on port", port);
+});
